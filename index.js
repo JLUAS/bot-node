bd075655fa790bb5211dea34763c1af38c34711c
JLUAS <jlurquieta1@gmail.com>
2024-12-11 23:38:46 -0600

    hola

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ io.on("connection", (socket) => {
           }),
         ]);
         const botResponse = gptResponse.choices[0].message.content;
-        io.emit("message", text)
+        socket.emit("message", botResponse)
       } catch (error) {
         io.emit("error", { message: "Error al procesar la solicitud." });
       }
